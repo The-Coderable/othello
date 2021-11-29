6cfade81362d0176dfbb5525851e503622b0e713
Coderable <84838028+The-Coderable@users.noreply.github.com>
2021-11-29 13:20:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,15 +22,7 @@ function App(props) {
   return (<div className="App">
     <Othello/>
     <div>
-      <a tabIndex={0} href="https://github.com/blueedgetechno/othello" rel="noopener noreferrer" target="_blank" className="github-corner" aria-label="View source on GitHub">
-        <svg width={80} height={80} className="github-badge" viewBox="0 0 250 250" style={{
-            fill: '#151513',
-            color: '#fff',
-            position: 'absolute',
-            top: 0,
-            border: 0,
-            right: 0
-          }} aria-hidden="true">
+     
           <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"/>
           <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style={{
               transformOrigin: '130px 106px'
@@ -43,7 +35,7 @@ function App(props) {
       {theme=="light"?<SunIcon/>:<MoonIcon/>}
     </div>
     <div className="unescapeLink">
-      <a href="https://blueedge.me/unescape" target="_blank">UNESCAPE 🎮</a>
+      <a href="https://coderable.me" target="_blank">Coderable</a>
     </div>
   </div>);
 }
